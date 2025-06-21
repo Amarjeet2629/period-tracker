a8494883b984787c675aaa8d27e94eea3e9ee543
Amarjeet Sinha <amarjeet@stripe.com>
2025-06-21 23:47:21 +0530

    init code for deployment on vercel

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,18 @@ const withPWA = withPWAInit({
   dest: 'public',
   register: true,
   skipWaiting: true,
+  runtimeCaching: [
+    {
+      urlPattern: /^https?.*/,
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: 'offlineCache',
+        expiration: {
+          maxEntries: 200,
+        },
+      },
+    },
+  ],
   disable: process.env.NODE_ENV === 'development',
 });
 
