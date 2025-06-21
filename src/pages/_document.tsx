9fb9583fd288f317f21354862b88346c471ddeed
Amarjeet Sinha <amarjeet@stripe.com>
2025-06-21 23:43:17 +0530

    init code for PWA

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icons/icon512_rounded.png" />
+        <meta name="theme-color" content="#8936FF" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
