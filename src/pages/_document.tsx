32a2f2d31e05d310d6c77e21e1a873cfc7ee1337
Amarjeet Sinha <amarjeet@stripe.com>
2025-06-22 20:48:47 +0530

    make changes to manifest

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,13 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
+  const cacheBuster = `?v=${new Date().getTime()}`;
   return (
     <Html lang="en">
       <Head>
-        <link rel="manifest" href="/manifest.json" />
+        <link rel="manifest" href={`/manifest.json${cacheBuster}`} />
         <link rel="apple-touch-icon" href="/icons/icon512_rounded.png" />
-        <meta name="theme-color" content="#8936FF" />
+        <meta name="theme-color" content="#000000" />
       </Head>
       <body className="antialiased">
         <Main />
